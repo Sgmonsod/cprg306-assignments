7391c06cd58a1dfd0a56f3d1304ae4ed468afa37
Sgmonsod <SamuelJesse.Monsod@edu.sait.ca>
2024-05-27 09:19:52 -0600

    first commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+    <main>
+    <h1>Samuel Jesse</h1>
+    <nav><Link href = "https://github.com/Sgmonsod/cprg306-assignments.git">GitHub</Link></nav>
+    
+    </main>
+    ) 
+};
